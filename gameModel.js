bd574029ecab94d3fbe020dec03961c483f786d6
Hilly Jackobson <hjackobson@hjackobson.dev.drivenets.net>
2018-03-28 10:32:16 +0300

    changing style.background to class hover

--- gameModel.js
+++ gameModel.js
@@ -12,55 +12,21 @@ circleModel.prototype = {
 };
 
 
-let circleArrModel = function (arrayNumber){
-    this.circleArr = [];
-    this.arrNum = arrayNumber;
-    this.arrCounter = 0;
-};
-
-circleArrModel.prototype = {
-    addCircle: function(){
-        this.circleArr.push(new circleModel())
-    },
-    click: function(playerColor){
-        this.circleArr[this.arrCounter].changeColor(playerColor);
-        this.arrCounter++;
-    },
-    deleteCircle: function () {
-        this.circleArr[this.arrCounter].deleteColor();
-        this.arrCounter--;
-    }
-
-};
-
 let GameBoardModel = function (){
     this.gameBoard = [];
 };
-GameBoardModel.prototype.clear = function () {
-    this.gameBoard = [];
-}
-GameBoardModel.prototype = {
-    addCircleArr: function(){
-        this.gameBoard.push(new circleArrModel(this.gameBoard.length))
-    },
-    deleteGameBoard: function () {
 
-    }
+GameBoardModel.prototype = {
+    clear: function () {
+        this.gameBoard = [];
+    },
+    initGameBoard: function (rows, columns) {
+        for(let i = 0; i < rows; i++){
+            gameBoard[i] = []
+            for(let j = 0; j < columns; j++){
+                gameBoard[i][j] = null;
+            }
+        }
+    },
 
 };
-
-
-
-
-
-let gameBoard = [];
-
-let memsetGameBoard = function (rows, columns) {
-    for(let i = 0; i < rows; i++){
-        gameBoard[i] = []
-        for(let j = 0; j < columns; j++){
-            gameBoard[i][j] = null;
-        }
-    }
-}
-
